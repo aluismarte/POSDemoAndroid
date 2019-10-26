6b91aede3772e77cc07d82de2d5838f4f2616844
Aluis Marte <aluismarte@gmail.com>
2019-10-26 17:27:23 -0400

    Subiendo el inicio

---
+++ app/src/main/java/com/alsjava/courses/posdemoandroid/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.alsjava.courses.posdemoandroid
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnMiBoton.setOnClickListener {
+            Toast.makeText(this, "Mi dieron click", Toast.LENGTH_LONG).show()
+        }
+    }
+}
